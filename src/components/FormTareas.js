49d47dc0eecd59d6c944f769deb329c60b811cf1
Estebanmarquez-1 <tebi_cba1@hotmail.com>
2021-11-04 23:05:56 -0300

    se agregaron functional components ademas de agregar logica y avanzar con el formulario

--- src/components/FormTareas.js
+++ src/components/FormTareas.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import ListaTareas from "./ListaTareas";
 
 const FormTareas = () => {
+  // aqui va la logica
+  // crear los state necesarios
+  const [tareaIndividual, setTareaIndividual] = useState("");
+  const [tareas, setTareas] = useState([]);
+
+  // const guardarTarea = (e) => {
+  //   // console.log(e.target.value)
+  //   setTareaIndividual(e.target.value)
+  // }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    // console.log("Desde handleSubmit")
+    setTareas([...tareas, tareaIndividual])
+  };
+
   return (
+    // aqui va el maquetado y muy poca logica
     <>
-      <form className= "container my-5">
+      <form className="container my-5" onSubmit={handleSubmit}>
         <div className="mb-3 d-flex">
-          <input type="text" placeholder="Ingrese una tarea" className= "form-control"/>
-          <button type= "submit" className= "btn btn-outline-light">Agregar</button>
+          <input
+            type="text"
+            placeholder="Ingrese una tarea"
+            className="form-control"
+            onChange = { e => setTareaIndividual(e.target.value)}
+          />
+          <button type="submit" className="btn btn-outline-light">
+            Agregar
+          </button>
         </div>
       </form>
-      <section className= "container">
-        <ListaTareas/>
+      <section className="container">
+        <ListaTareas />
       </section>
     </>
   );
